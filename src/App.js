817a71df57a3c7a878129e0d1a1b47f0a2418374
imanardiansyahh <imanardiansyah89@gmail.com>
2020-10-06 20:54:53 +0800

    tambah

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../src/components/navbar'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
 
 
 import './App.css';
@@ -11,7 +12,7 @@ function App() {
     <Router>
     <Navbar/>
     <Switch>
-    <Route path='/' exact/>
+    <Route path='/' exact component={Home}/>
     </Switch>
     </Router>
     </>
